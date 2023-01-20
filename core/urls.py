453bd2b5b6b6c3970a958521e161e1899ed44240
user <azamataliaydarov20@gmail.com>
2023-01-20 14:43:13 +0100

    info

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('gym_admin/', view='index', name='index')
 ]
 
 admin.site.index_title = "Admin page"
